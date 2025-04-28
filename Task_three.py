5774162cc8cf3c2fe948d5f697a12fd29f08b077
RKrauja <rk@fairytalesgroup.com>
2025-04-28 15:49:57 +0200

    Task three

--- Task_three.py
+++ Task_three.py
@@ -1,13 +1,17 @@
 import machine
 import network
 import socket
+import json
 
 ap = network.WLAN(network.AP_IF)  # Create a WLAN object and assign it to a variable
 ap.active(True)  # Activate the access point
 ap.config(essid='Shawarma 24')  # Set the network name
 ap.config(authmode=3, password='LielsDibens')  # Set the authentication mode and password
 
-pins = [machine.Pin(i, machine.Pin.IN) for i in (12, 22, 23, 34)]
+#pins = [machine.Pin(i, machine.Pin.IN) for i in (12, 22, 23, 34)]
+pin_numbers = (12, 22, 23, 34)
+sensor_names = ['button', 'temp_sensor', 'potentiometer']
+pins = {pin_num: machine.Pin(pin_num, machine.Pin.IN) for pin_num in pin_numbers}
 
 temp_sensor = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(23))
 button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
@@ -18,15 +22,23 @@ potentiometer.atten(machine.ADC.ATTN_11DB)
 address = 24
 temp_reg = 5
 
-# Define an HTML template for displaying the pin values as a table tag
-html = """<!DOCTYPE html>
-<html>
-    <head> <title>ESP32 Pins</title> </head>
-    <body> <h1>ESP32 Pins</h1>
-        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
-    </body>
-</html>
-"""
+def generate_html(pins, temperature, pot_input):
+    # This will create the table rows
+    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(pin_num), pins[pin_num].value()) for pin_num in pins]
+    rows.append('<tr><td>Temperature</td><td>%.2f°C</td></tr>' % temperature)
+    rows.append('<tr><td>Potentiometer</td><td>%d µV</td></tr>' % pot_input)
+    
+    # Define an HTML template for displaying the pin values as a table tag
+    html = """<!DOCTYPE html>
+    <html>
+        <head> <title>ESP32 Pins</title> </head>
+        <body> <h1>ESP32 Pins</h1>
+            <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+        </body>
+    </html>
+    """
+
+    return html % '\n'.join(rows) 
 
 # Get the address information of the server
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
@@ -55,24 +67,104 @@ def read_temp():
 def read_pot():
     return potentiometer.read_uv()
 
+def handle_pin(cl, pin_id):
+    if pin_id in pins:
+        pin = pins[pin_id]
+        response = {
+            "value": pin.value()
+        }
+    else:
+        response = {"error": f"Pin {pin_id} not found."}
 
+    json_data = json.dumps(response)
+    cl.send('HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n')
+    cl.send(json_data)
+
+def load_all_pins():
+    response = {
+        "pins": pins
+    }
+    json_data = json.dumps(response)
+    cl.send('HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n')
+    cl.send(json_data)
 
 while True:
     cl, addr = s.accept()  # Accept a connection
     print('client connected from', addr)
-    cl_file = cl.makefile('rwb',
-                          0)  # Create a file-like (behaves further just like a file) object with permission to read and write, as well as a buffer size of 0
+    cl_file = cl.makefile('rwb',0)  # Create a file-like (behaves further just like a file) object with permission to read and write, as well as a buffer size of 0
+    handled = False
+
     while True:
         temperature = read_temp()
         line = cl_file.readline()  # Read a line from the client request
         pot_input = read_pot()
         print(f"Current line: {line}")
+
         if not line or line == b'\r\n':  # Check if the line is empty or contains only a newline
-            break  # If true break the inner loop
-    # Create a list with html tags of table rows containing pins and their values
-    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
-    rows.append('<tr><td>%s</td><td>%d</td></tr>' % ('Temperature', temperature))
-    rows.append('<tr><td>%s</td><td>%d</td></tr>' % ('Potentiometer', pot_input))
-    response = html % '\n'.join(rows)  # Format the HTML template with the new rows
-    cl.send(response)  # Send the response to the client
-    cl.close()  # Close the connection
+                break  # If true break the inner loop
+        
+        if 'GET /pins/' in line:
+                request_line = line.decode().split(' ')[1]  # Get the URL path, like /pins/12
+                pin_str = request_line.split('/')[2] #12
+                print(pin_str)
+                try:
+                    pin_id = int(pin_str)
+                    handle_pin(cl, pin_id)
+                except:
+                     load_all_pins()
+                handled = True
+
+        elif 'GET /pins' in line:
+            load_all_pins()
+            handled = True
+
+        elif 'GET /sensors' in line:
+                response = {
+                    "sensors": sensor_names 
+                }
+                json_data = json.dumps(response)
+                cl.send('HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n')
+                cl.send(json_data)
+                handled = True
+
+
+        elif 'GET /sensor/' in line:
+            request_line = line.decode().split(' ')[1]  # Get the URL path, like /sensor/button
+            sensor_str = request_line.split('/')[2] # temp_sensor
+            
+            if sensor_str == 'temp_sensor':
+                temp = read_temp()
+                response = {
+                    "Value": temp 
+                }
+                json_data = json.dumps(response)
+                cl.send('HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n')
+                cl.send(json_data)
+                handled = True
+
+            elif sensor_str == 'potentiometer':
+                pot = read_pot()
+                response = {
+                    "Value": pot 
+                }
+                json_data = json.dumps(response)
+                cl.send('HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n')
+                cl.send(json_data)
+                handled = True
+
+            elif sensor_str == 'button':
+                pin_id = 12
+                handle_pin(cl, pin_id)
+                handled = True
+            
+            else:
+                cl.send('HTTP/1.1 404 Not Found\r\n\r\nSensor not found')    
+                handled = True
+
+    if not handled:
+        # Only generate and send HTML if no JSON was already sent
+        response = generate_html(pins, temperature, pot_input)
+        cl.send('HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n')
+        cl.send(response)
+
+    cl.close()
